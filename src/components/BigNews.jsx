db2f4c2683843597ab001e5f846a2dc9e9079e41
Matteo Minin <matteominin@MacBook-Pro-di-Matteo-2.local>
2025-04-18 16:47:30 +0200

    Font size fix

--- src/components/BigNews.jsx
+++ src/components/BigNews.jsx
@@ -9,7 +9,7 @@ const BigNews = ({ category }) => {
             </div>
             <div className="big-news__content">
                 <h1>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
                 </h1>
                 <div className="big-news__content__row"></div>
                 <p className='big-news__content__text'>
